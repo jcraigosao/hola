57261be43fe1b2f0ec6bf07644afde07dfc0130e
Estudiante <Estudiante@10.203.1.235>
2017-02-06 15:32:49 -0500

    Vectores y Funciones

--- src/X.java
+++ src/X.java
@@ -10,7 +10,28 @@
  */
 public class X {
     public static void  main(String [] arg){
-
-           System.out.println ("Hola mundo, Cristian");
+       java.util.Scanner lectura = new java.util.Scanner(System.in);
+        int d[] = new int[5];
+        for (int i = 0; i < 5; i++) {
+             System.out.println("Ingrese " + i);
+             d[i]= lectura.nextInt();
+        }
+       
+        
+        int r = suma(d);
+        System.out.println(r);
+               
+          
 }
+    
+public static int suma(int[] vector){
+              
+              int r=0;
+              for(int i: vector)
+                  r+= i;
+                          
+              return r;
+          }
+
+
 }
